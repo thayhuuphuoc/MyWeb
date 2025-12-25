d68234dc5269cd1abff594ae646fc5b8104a1a28
nguyenhuuphuoc <nhphuoc103@gmail.com>
2025-12-25 07:32:54 +0700

    feat: update table feature with quill-table-better and fix table border color

--- config/quill-config.ts
+++ config/quill-config.ts
@@ -4,7 +4,7 @@ import Quill from "quill"
 import {uploadFile} from "@/lib/image-data";
 import hljs from "highlight.js";
 import {ImageResize} from "quill-image-resize-module-ts";
-import QuillTableBetter from "quill-table-better"
+import QuillTableBetter from "quill-table-better";
 
 let icons: any = Quill.import('ui/icons');
 icons['custom-code'] = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-code-2"><path d="M4 22h14a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v4"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="m5 12-3 3 3 3"/><path d="m9 18 3-3-3-3"/></svg>';
@@ -13,7 +13,7 @@ hljs.configure({
 	languages: ['html', 'css', 'javascript', 'php', 'python', 'typescript'],
 })
 
-Quill.register({'modules/table-better': QuillTableBetter}, true)
+Quill.register({ "modules/table-better": QuillTableBetter }, true)
 Quill.register('modules/imageResize', ImageResize);
 
 export const QuillConfig = {
@@ -23,7 +23,6 @@ export const QuillConfig = {
 			['bold', 'italic', 'underline', 'strike'],        // toggled buttons
 			['blockquote', 'code-block'],
 			['link', 'image', 'image-url', 'video', 'formula'],
-			['table-better'],                                 // insert table button
 
 			[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
 			[{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
@@ -36,6 +35,7 @@ export const QuillConfig = {
 			[{ 'font': [] }],
 			[{ 'align': [] }],
 
+			['table-better'], // Add table tool
 			['clean', 'custom-code']                                         // remove formatting button
 		],
 		handlers: {
@@ -120,18 +120,18 @@ export const QuillConfig = {
 	clipboard: {
 		matchVisual: false
 	},
-	table: false,  // disable default table module
-	'table-better': {
-		language: 'en_US',
+	table: false,  // disable table module
+	"table-better": {
+		language: "en_US",
 		menus: [
-			'column',
-			'row',
-			'merge',
-			'table',
-			'cell',
-			'wrap',
-			'copy',
-			'delete',
+			"column",
+			"row",
+			"merge",
+			"table",
+			"cell",
+			"wrap",
+			"copy",
+			"delete",
 		],
 		toolbarTable: true,
 	},
