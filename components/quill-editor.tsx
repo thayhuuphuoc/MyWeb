51d32095e30df0f84469d32cac7342b0d7bd6406
nguyenhuuphuoc <nhphuoc103@gmail.com>
2025-12-31 07:13:14 +0700

    Add default border to table when created via insertTable
    - Monkey-patch insertTable method to set border-style, border-color, border-width on new tables
    - Set border properties separately (not shorthand) so getElementStyle can read them
    - Add text-change listener to ensure all tables have border style
    - Border is set as inline style: border-style: solid; border-color: #000000; border-width: 1px
    - This makes table properties form show default border (solid, black, 1px) like cell form
    - Consistent with code sample approach of setting style when creating table

--- components/quill-editor.tsx
+++ components/quill-editor.tsx
@@ -3,6 +3,7 @@
 import React, {useEffect, useRef} from "react";
 import '@/styles/quill/quill.css'
 import 'quill-table-better/dist/quill-table-better.css'
+import '@/styles/quill/table-custom.css'
 import 'highlight.js/styles/github-dark-dimmed.min.css'
 import Quill from "quill";
 import {QuillConfig} from "@/config/quill-config";
@@ -29,128 +30,65 @@ const QuillEditor = React.forwardRef<HTMLTextAreaElement, TextareaProps>((props,
 			modules: QuillConfig
 		})
 
+		// Monkey-patch insertTable to add default border to style
+		const tableBetter = quill.getModule('table-better')
+		if (tableBetter && tableBetter.insertTable) {
+			const originalInsertTable = tableBetter.insertTable.bind(tableBetter)
+			tableBetter.insertTable = function(rows: number, columns: number) {
+				// Call original method
+				const result = originalInsertTable(rows, columns)
+				
+				// Set border style on the table element after it's created
+				// Set border properties separately so getElementStyle can read them
+				const setBorder = () => {
+					const tables = quill.root.querySelectorAll('table')
+					if (tables.length > 0) {
+						const lastTable = tables[tables.length - 1] as HTMLElement
+						const currentStyle = lastTable.getAttribute('style') || ''
+						// Only set if border-style is not already in style
+						if (!currentStyle.includes('border-style') && !currentStyle.includes('border:')) {
+							// Set border properties separately so getElementStyle can read them
+							const newStyle = currentStyle 
+								? `${currentStyle}; border-style: solid; border-color: #000000; border-width: 1px`
+								: 'border-style: solid; border-color: #000000; border-width: 1px'
+							lastTable.setAttribute('style', newStyle)
+							return true
+						}
+					}
+					return false
+				}
+				
+				// Try multiple times to catch table creation
+				if (!setBorder()) {
+					setTimeout(() => {
+						if (!setBorder()) {
+							setTimeout(setBorder, 50)
+						}
+					}, 10)
+				}
+				
+				return result
+			}
+		}
+
+		// Also listen to text-change to ensure border is set for all tables
+		quill.on('text-change', () => {
+			const tables = quill.root.querySelectorAll('table')
+			tables.forEach((table) => {
+				const tableEl = table as HTMLElement
+				const currentStyle = tableEl.getAttribute('style') || ''
+				if (!currentStyle.includes('border-style') && !currentStyle.includes('border:')) {
+					const newStyle = currentStyle 
+						? `${currentStyle}; border-style: solid; border-color: #000000; border-width: 1px`
+						: 'border-style: solid; border-color: #000000; border-width: 1px'
+					tableEl.setAttribute('style', newStyle)
+				}
+			})
+		})
+
 		quill.setContents(quill.clipboard.convert({html: props.value || props.defaultValue}), 'silent')
 
 		quill.on('editor-change', () => props.onChange(quill.getSemanticHTML()))
-
-		// Set default border values for table form
-		const tableModule = quill.getModule('table-better')
-		if (tableModule && tableModule.tableMenus) {
-			const tableMenus = tableModule.tableMenus
-			
-			// Store original tablePropertiesForm
-			let _tablePropertiesForm = tableMenus.tablePropertiesForm || null
-			
-			// Intercept setter to modify attribute and update UI
-			Object.defineProperty(tableMenus, 'tablePropertiesForm', {
-				set: function(newForm) {
-					if (newForm && newForm.options && newForm.options.type === 'table') {
-						// Modify attribute to set defaults
-						const attrs = newForm.options.attribute || {}
-						
-						// Set default border-style to 'solid' if not set or is 'none'
-						if (!attrs['border-style'] || attrs['border-style'] === '' || attrs['border-style'] === 'none') {
-							attrs['border-style'] = 'solid'
-						}
-						
-						// Set default border-color to '#000000' if not set
-						if (!attrs['border-color'] || attrs['border-color'] === '') {
-							attrs['border-color'] = '#000000'
-						}
-						
-						// Set default border-width to '1px' if not set
-						if (!attrs['border-width'] || attrs['border-width'] === '') {
-							attrs['border-width'] = '1px'
-						}
-						
-						// Update options.attribute
-						newForm.options.attribute = attrs
-						
-						// Update attrs (form constructor already copied, so we need to update it)
-						newForm.attrs = { ...newForm.attrs, ...attrs }
-						
-						// Use setAttribute method to update UI properly
-						const updateFormUI = () => {
-							if (!newForm.form || !newForm.form.parentNode) return false
-							
-							// Use form's setAttribute method to update values
-							if (newForm.setAttribute) {
-								if (attrs['border-style']) {
-									newForm.setAttribute('border-style', attrs['border-style'])
-								}
-								if (attrs['border-color']) {
-									newForm.setAttribute('border-color', attrs['border-color'])
-								}
-								if (attrs['border-width']) {
-									newForm.setAttribute('border-width', attrs['border-width'])
-								}
-							}
-							
-							// Also update UI directly as fallback
-							const borderRow = newForm.form.querySelector('.properties-form-row')
-							if (borderRow) {
-								// Update dropdown text
-								const dropText = borderRow.querySelector('.ql-table-dropdown-text') as HTMLElement
-								if (dropText && attrs['border-style']) {
-									dropText.innerText = attrs['border-style']
-								}
-								
-								// Update color
-								const colorInput = borderRow.querySelector('.label-field-view-color .property-input') as HTMLInputElement
-								const colorButton = borderRow.querySelector('.color-button') as HTMLElement
-								if (colorInput && attrs['border-color']) {
-									colorInput.value = attrs['border-color']
-									if (colorButton) {
-										colorButton.style.backgroundColor = attrs['border-color']
-										colorButton.classList.remove('color-unselected')
-									}
-								}
-								
-								// Update width
-								const allInputs = borderRow.querySelectorAll('.property-input')
-								if (allInputs.length > 1) {
-									const widthInput = allInputs[1] as HTMLInputElement
-									if (widthInput && attrs['border-width']) {
-										widthInput.value = attrs['border-width']
-									}
-								}
-								
-								// Enable inputs
-								const colorContainer = borderRow.querySelector('.ql-table-color-container') as HTMLElement
-								if (colorContainer) {
-									colorContainer.classList.remove('ql-table-disabled')
-								}
-								const allContainers = borderRow.querySelectorAll('.label-field-view')
-								if (allContainers.length > 1) {
-									const widthContainer = allContainers[1] as HTMLElement
-									if (widthContainer) {
-										widthContainer.classList.remove('ql-table-disabled')
-									}
-								}
-							}
-							
-							return true
-						}
-						
-						// Try to update UI with retries
-						if (!updateFormUI()) {
-							setTimeout(() => {
-								if (!updateFormUI()) {
-									setTimeout(updateFormUI, 50)
-								}
-							}, 10)
-						}
-					}
-					
-					_tablePropertiesForm = newForm
-				},
-				get: function() {
-					return _tablePropertiesForm
-				},
-				configurable: true,
-				enumerable: true
-			})
-		}
 	}, []);
 
 	return (
